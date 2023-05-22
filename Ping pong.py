2ee05283a2d0ff76f0e3d6826bd0ff530792236e
Rino0709123 <130834275+Rino0709123@users.noreply.github.com>
2023-05-22 16:10:35 +0300

    Add files via upload

---
+++ Ping pong.py
@@ -0,0 +1,132 @@
+from pygame import *
+from random import randint
+
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (50, 50))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+class GameSprite2(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (20, 120))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+       
+class Player(GameSprite2):
+
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.x > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN]: #and self.rect.x < win_width - 80
+           self.rect.y += self.speed
+   def fire(self):
+       bullet = Bullet('bullet.png', self.rect.x, self.rect.top, 10)
+       bullets.add(bullet)
+
+
+class Player2(GameSprite2):
+
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.x > 5:
+           self.rect.y -= self.speed
+       if keys[K_s]:#and self.rect.x < win_width - 80
+           self.rect.y += self.speed
+   def fire(self):
+       bullet = Bullet('bullet.png', self.rect.x, self.rect.top, 10)
+       bullets.add(bullet)
+
+
+
+
+
+lost=0
+ball=0
+
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.kill()
+
+
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("Shooter Game")
+background = transform.scale(image.load("galaxy.jpg"), (win_width, win_height))
+
+
+player1 = Player('rocket.png', 665,  410, 4)
+player2 = Player2('rocket.png', 10,  210, 4)
+bullet=Bullet('bullet.png', 300,  410, 4)
+
+665,  410
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+font.init()
+font1 = font.Font(None, 35)
+font = font.Font(None, 70)
+win = font.render('YOU WIN!', True, (19, 255, 13))
+lose = font.render('YOU LOSE!', True, (240, 0, 0))
+
+
+
+while game:
+    text_lose = font1.render("Пропущено: " + str(lost), 1, (255, 255, 255))
+    text_ball = font1.render("Счёт: " + str(ball), 1, (255, 255, 255))
+    for e in event.get():
+        if e.type == QUIT:
+           game = False
+        
+
+  
+    if finish != True:
+        window.blit(background,(0, 0))
+        player1.update()
+        player2.update()
+        bullet.update()
+        
+        
+        player1.reset()
+        player2.reset()
+        bullet.reset()
+        #bullet.draw(window)
+        window.blit(text_lose, (5, 35))
+        window.blit(text_ball, (5, 5))
+
+           
+    '''if lost>=3:
+        finish = True
+        window.blit(lose, (210, 210))
+    if ball>=10:
+        finish = True
+        window.blit(win, (210, 210))   '''
+        
+        
+    '''if pygame.sprite.spritecollide(player1, bullet, True, True):
+        ball+=1
+    if pygame.sprite.spritecollide(player2, bullet, True, True):
+        ball+=1'''
+
+
+    display.update()
+    clock.tick(FPS)
